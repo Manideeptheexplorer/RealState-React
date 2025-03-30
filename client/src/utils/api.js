e50694c7147ccff85bc1de90e8c880f07714a9fa
Manideeptheexplorer <venkatkotha828@gmail.com>
2025-03-30 23:36:44 +0530

    vercel frontened

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 import {toast} from 'react-toastify';
 
 export const api=axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://real-state-react-iota.vercel.app/api",
 });
 
 export const getAllProperties=async()=>{
